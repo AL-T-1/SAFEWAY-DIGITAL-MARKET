0b0cb3b6854c24ee8543c2b494c16b8b5ae9161f
AL-T-1 <153546847+AL-T-1@users.noreply.github.com>
2023-12-18 21:35:47 +0300

    Update DigitalMarket.java
    first update

--- DigitalMarket.java
+++ DigitalMarket.java
@@ -1,3 +1,12 @@
+/* 
+SAFEWAY DIGITAL MARKET
+GROUP MEMBERS ON THE PROJECT
+    1) Abrham Adugna
+    2) Abyayle Ababaye
+    3) Adonay Kinfe
+    4) Aelaf Tsegaye
+    5) Alen Wenduwesun
+*/
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -44,142 +53,142 @@ public class DigitalMarket {
         }
     }
 
-    private static void introduction() {
-        System.out.println("Welcome to L Digital Market!");
-        System.out.println("Empowering Tomorrow's Digital World Today!\nSave your valuable time and money. Trade smarter!");
-    }
+private static void introduction() {
+    System.out.println("Welcome to L Digital Market!");
+    System.out.println("Empowering Tomorrow's Digital World Today!\nSave your valuable time and money. Trade smarter!");
+}
 
-    private static void serviceOptions() {
-        int option = 0;
-        while(option<=0 || option>=5) {
-            System.out.print("\nOptions:\n" +
-                "1. Service\n" +
-                "2. Terms and Conditions\n" +
-                "3. How to use\n" +
-                "4. About\n" +
-                "5. Exit\n" +
-                "Choose an option: ");
-        
-            Scanner scanner = new Scanner(System.in);
-            option = scanner.nextInt();
-
-            switch (option) {
-                case 1:
-                    service();
-                    break;
-                case 2:
-                    openTermsAndConditions();
-                    break;
-                case 3:
-                    openHowToUse();
-                    break;
-                case 4:
-                    aboutCompany();
-                    break;
-                case 5:
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid option. Please choose again.");
-            }
-        }
-        
-    }
-
-    private static void service() {
-        int options = 0;
-        while(options<=0 || options>=3) {
-            System.out.println("\nAre you :\n" +
-                    "1. Buyer\n" +
-                    "2. Seller\n" +
-                    "3. Admin\n" +
-                    "Choose an option: ");
+private static void serviceOptions() {
+    int option = 0;
+    while(option<=0 || option>=5) {
+        System.out.print("\nOptions:\n" +
+            "1. Service\n" +
+            "2. Terms and Conditions\n" +
+            "3. How to use\n" +
+            "4. About\n" +
+            "5. Exit\n" +
+            "Choose an option: ");
     
-            Scanner scanner = new Scanner(System.in);
-            options = scanner.nextInt();
-            switch (options) {
-                case 1:
-                    buyerService();
-                    break;
-                case 2:
-                    sellerService();
-                    break;
-                case 3:
-                    adminService();
-                    break;
-                default:
-                    System.out.println("Invalid option. Please choose again.");
-                    continue;
-            }
+        Scanner scanner = new Scanner(System.in);
+        option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                service();
+                break;
+            case 2:
+                openTermsAndConditions();
+                break;
+            case 3:
+                openHowToUse();
+                break;
+            case 4:
+                aboutCompany();
+                break;
+            case 5:
+                System.exit(0);
+            default:
+                System.out.println("Invalid option. Please choose again.");
         }
     }
+    
+}
 
-    private static void buyerService() {
-        // Implement buyerService based on the provided C++ code...
-    }
+private static void service() {
+    int options = 0;
+    while(options<=0 || options>=3) {
+        System.out.println("\nAre you :\n" +
+                "1. Buyer\n" +
+                "2. Seller\n" +
+                "3. Admin\n" +
+                "Choose an option: ");
 
-    private static void sellerService() {
-        // Implement sellerService based on the provided C++ code...
+        Scanner scanner = new Scanner(System.in);
+        options = scanner.nextInt();
+        switch (options) {
+            case 1:
+                buyerService();
+                break;
+            case 2:
+                sellerService();
+                break;
+            case 3:
+                adminService();
+                break;
+            default:
+                System.out.println("Invalid option. Please choose again.");
+                continue;
+        }
     }
+}
 
-    private static void adminService() {
-        // Implement adminService based on the provided C++ code...
-    }
+private static void buyerService() {
+    // Implement buyerService based on the provided C++ code...
+}
 
-    private static void openTermsAndConditions() {
-        // Implement openTermsAndConditions based on the provided C++ code...
-    }
+private static void sellerService() {
+    // Implement sellerService based on the provided C++ code...
+}
 
-    private static void openHowToUse() {
-        // Implement openHowToUse based on the provided C++ code...
-    }
+private static void adminService() {
+    // Implement adminService based on the provided C++ code...
+}
 
-    private static void aboutCompany() {
-        // Implement aboutCompany based on the provided C++ code...
-    }
+private static void openTermsAndConditions() {
+    // Implement openTermsAndConditions based on the provided C++ code...
+}
 
-    private static void saveBuyerData(List<Buyer> buyers) {
-        // Implement saveBuyerData based on the provided C++ code...
-    }
+private static void openHowToUse() {
+    // Implement openHowToUse based on the provided C++ code...
+}
 
-    private static void saveSellerData(List<Seller> sellers) {
-        // Implement saveSellerData based on the provided C++ code...
-    }
+private static void aboutCompany() {
+    // Implement aboutCompany based on the provided C++ code...
+}
 
-    private static void saveProductData(List<Product> products) {
-        // Implement saveProductData based on the provided C++ code...
-    }
+private static void saveBuyerData(List<Buyer> buyers) {
+    // Implement saveBuyerData based on the provided C++ code...
+}
 
-    private static List<Buyer> loadBuyerData() {
-        // Implement loadBuyerData based on the provided C++ code...
-        return new ArrayList<>();  // Placeholder, replace with actual implementation
-    }
+private static void saveSellerData(List<Seller> sellers) {
+    // Implement saveSellerData based on the provided C++ code...
+}
 
-    private static List<Seller> loadSellerData() {
-        // Implement loadSellerData based on the provided C++ code...
-        return new ArrayList<>();  // Placeholder, replace with actual implementation
-    }
+private static void saveProductData(List<Product> products) {
+    // Implement saveProductData based on the provided C++ code...
+}
 
-    private static List<Product> loadProductData() {
-        // Implement loadProductData based on the provided C++ code...
-        return new ArrayList<>();  // Placeholder, replace with actual implementation
-    }
+private static List<Buyer> loadBuyerData() {
+    // Implement loadBuyerData based on the provided C++ code...
+    return new ArrayList<>();  // Placeholder, replace with actual implementation
+}
 
-    private static void displayProductTable(List<Product> products) {
-        // Implement displayProductTable based on the provided C++ code...
-    }
+private static List<Seller> loadSellerData() {
+    // Implement loadSellerData based on the provided C++ code...
+    return new ArrayList<>();  // Placeholder, replace with actual implementation
+}
 
-    private static void buyProduct(List<Product> products, List<Buyer> buyers) {
-        // Implement buyProduct based on the provided C++ code...
-    }
+private static List<Product> loadProductData() {
+    // Implement loadProductData based on the provided C++ code...
+    return new ArrayList<>();  // Placeholder, replace with actual implementation
+}
 
-    private static int findBuyerIndex(int accountNumber) {
-        // Implement findBuyerIndex based on the provided C++ code...
-        return -1;  // Placeholder, replace with actual implementation
-    }
+private static void displayProductTable(List<Product> products) {
+    // Implement displayProductTable based on the provided C++ code...
+}
 
-    private static void displayBuyerTable(List<Buyer> buyers) {
-        // Implement displayBuyerTable based on the provided C++ code...
-    }
+private static void buyProduct(List<Product> products, List<Buyer> buyers) {
+    // Implement buyProduct based on the provided C++ code...
+}
+
+private static int findBuyerIndex(int accountNumber) {
+    // Implement findBuyerIndex based on the provided C++ code...
+    return -1;  // Placeholder, replace with actual implementation
+}
+
+private static void displayBuyerTable(List<Buyer> buyers) {
+    // Implement displayBuyerTable based on the provided C++ code...
+}
 
     private static void displaySellerTable(List<Seller> sellers) {
         // Implement displaySellerTable based on the provided C++ code...
